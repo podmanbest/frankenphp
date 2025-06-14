35f1a29b71c5fee9765b809e422cbde71c45dea4
Asep Ready <savalunova@gmail.com>
2025-06-14 06:08:52 -0400

    latest

--- run.sh
+++ run.sh
@@ -1,22 +1,10 @@
 #!/bin/sh
-if [ ! -d "data/*" ]; then
-  mkdir -p data/mysql data/logs/mysql
-fi
-
-# Check Pods
-if podman pod exists laravel ; then
-    podman pod rm -f laravel 
-fi
-podman pod create --name laravel -p 8080:80
-
-# Remove old images
-if podman image exists php; then
-  podman rmi -f app 2>/dev/null || true
-fi
-
 # Build images
-sh buildah.sh
+
+podman build -t frankenphp:1.7.0 ./1.7.0
 
 # Run test
 podman run -dt --name frankenphp -p 80:80 frankenphp:1.7.0
 
+# Login 
+podman exec -it frankenphp sh
